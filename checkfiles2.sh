26f58ea430246b514f6584e5ca6322be63b846f9
AWS ParallelCluster user <ec2-user@ip-10-0-6-188.ap-southeast-1.compute.internal>
2026-09-01 09:23:22 +0000

    Add Lab 5 just-in-time submission scripts

---
+++ checkfiles2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Number of hkl files"
+find . -name "*.hkl" | grep -v -e spiketrain -e mountains | wc -l
+echo "Number of mda files"
+find mountains -name "firings.mda" | wc -l
+echo "#==========================================================="
+echo "Start Times"
+head -n 2 *slurm*.out
+echo "End Times"
+tail -n 5 *slurm*.out
+echo "#==========================================================="
---
+++ consol_jobs.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bash /data/src/PyHipp/ec2snapshot.sh
