f71b8769a095b39fc66fd4debdd7919f8d8793ce
AWS ParallelCluster user <ec2-user@ip-10-0-2-205.ap-southeast-1.compute.internal>
2026-09-04 05:36:57 +0000

    Complete envlist.py logic and apply dynamic envs to highpass script

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -7,6 +7,11 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
+
 python -u -c "import PyHipp as pyh; \
 import time; \
 pyh.RPLHighPass(saveLevel=1); \
@@ -15,3 +20,7 @@ mountain_batch.mountain_batch(); \
 from PyHipp import export_mountain_cells; \
 export_mountain_cells.export_mountain_cells(); \
 print(time.localtime());"
+
+conda deactivate
+/data/src/PyHipp/envlist.py $envarg
+
